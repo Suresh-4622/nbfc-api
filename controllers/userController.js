8afd50482bac7292f8f75073e136008c84155ed6
Suresh-4622 <138596848+Suresh-4622@users.noreply.github.com>
2023-10-04 16:31:07 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,15 @@
-import User from "../models/userModel.js";
-import { TOKEN_KEY } from "../config.js";
+import Branch from "../models/branchModel.js";
+import { getAll, getOne, deleteOne } from "./baseController.js";
 import moment from "moment-timezone";
-import jwt from "jsonwebtoken";
 
-export async function userRegistration(req, res, next) {
+export async function createBranch(req, res, next) {
   try {
     const data = req.body;
 
     const missingFields = [];
 
-    const requiredFields = ["userName" ,"email", "password"];
+    const requiredFields = ["orgId", "branchName", "branchCode", "branchPhone", 
+    "branchEmail", "address", "state", "city", "pincode"];
     for (const field of requiredFields) {
       if (!data[field]) {
         missingFields.push(field);
@@ -17,121 +17,116 @@ export async function userRegistration(req, res, next) {
     }
 
     if (missingFields.length > 0) {
-      return res.status(422).json({
-        status: false,
-        message: `${missingFields} is required fields`,
+      return res.status(400).json({
+        message: "Missing required fields",
+        missingFields,
       });
     }
-    const checkUserExist = await User.find({ email: data.email });
-    const date = Date.now();
-    const createdAt = moment(date).format("lll");
 
-    if (checkUserExist.length === 0) {
-      const userData = await User.create({
-        userName: data.userName,
-        email: data.email,
-        password: data.password,
-        isFirst: true,
-        createdAt: createdAt,
+    const checkExistBranch = await Branch.find({
+      orgId: data.orgId,
+      branchCode: data.branchCode,
+    });
+
+    if (checkExistBranch.length === 0) {
+      const date = Date.now();
+      const createAt = moment(date).format("lll");
+
+      const branchData = await Branch.create({
+        userId: data.userId,
+        orgId: data.orgId,
+        branchName: data.branchName,
+        branchCode: data.branchCode,
+        branchPhone: data.branchPhone,
+        branchEmail: data.branchEmail,
+        address: data.address,
+        state: data.state,
+        city: data.city,
+        pincode: data.pincode,
+        createdBy: data.userId,
+        createdAt: createAt,
       });
-
-      const responseUserData = {
-        userName: userData.userName,
-        email: userData.email,
-        createdAt: userData.createdAt,
-        id: userData.id,
-      };
-
-      res.status(200).json({
-        status: true,
-       message: "Register Successfully",
+      res.status(201).json({
+        status: "Created",
+        message: "Branch Created Successfully",
+        branchData,
       });
     } else {
       res.status(208).json({
-        status: false,
-        message: "User Already Exist",
+        message: "Branch Already Exist",
+        checkExistBranch,
       });
     }
   } catch (error) {
-    if (error.name === "ValidationError") {
-      const validationErrors = {};
-      for (const field in error.errors) {
-        validationErrors[field] = error.errors[field].message;
-      }
-      const firstValidationErrorField = Object.keys(validationErrors)[0];
-      const errorMessage = validationErrors[firstValidationErrorField];
-
-      return res.status(422).json({
-        status: false,
-        message: errorMessage,
-      });
-    }
     next(error);
   }
 }
 
-
-export async function login(req, res, next) {
+export async function updateBranch(req, res, next) {
   try {
     const data = req.body;
 
-    const missingFields = [];
-
-    const requiredFields = ["email", "password"];
-    for (const field of requiredFields) {
-      if (!data[field]) {
-        missingFields.push(field);
-      }
-    }
-
-    if (missingFields.length > 0) {
-      return res.status(422).json({
-        status: false,
-        message: `${missingFields} is required fields`,
-      });
-    }
-
-    const user = await User.findOne({
-      email: data.email,
-      password: data.password,
-    });
-
     const date = Date.now();
+    const updateAt = moment(date).format("lll");
 
-    if (user) {
-      const tokenId = jwt.sign(
-        { email: data.email, password: data.password },
-        TOKEN_KEY,
-        {
-          expiresIn: "10m",
-        }
-      );
-      const userData= await User.findByIdAndUpdate(
-        { _id: user._id },
-        {
-          lastLogin: date,
-          token: tokenId,
-        },
-        {
-          new: true,
-          runValidators: true,
-          select: '-password -isFirst -id -token',
-        }
-      );
-      res.status(200).json({
-        status: true,
-        page: "wizard",
-        isFirst: user.isFirst,
-        userToken: userData.token,
-        userData,
-      });
-    } else {
-      res.status(400).json({
-        status: false,
-        message: "Invalid Credential",
-      });
-    }
+    const editData = {
+      clientId: data.clientId,
+      orgName: data.orgName,
+      orgCode: data.orgCode,
+      phone: data.phone,
+      orgEmail: data.orgEmail,
+      address: data.address,
+      state: data.state,
+      city: data.city,
+      pincode: data.pincode,
+      updatedBy: data.userId,
+      updatedAt: updateAt,
+    };
+
+    const updatedData = await Branch.findByIdAndUpdate(
+      data.branchId,
+      editData,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(201).json({
+      status: "Updated",
+      message: "Branch Updated Successfully",
+      updatedData,
+    });
   } catch (error) {
     next(error);
   }
 }
+export async function getAllBranch(req, res, next) {
+  try {
+    const data = await Branch.find().populate("userId").populate("orgId");
+    res.status(201).json({
+      status: "Success",
+      message: "Get All Branch Successfully",
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getBranch(req, res, next) {
+  try {
+    const branchId = req.params.id;
+    const data = await Branch.findOne({ _id: branchId })
+      .populate("userId")
+      .populate("orgId");
+    res.status(201).json({
+      status: "Success",
+      message: "Get All Branch Successfully",
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const deleteBranch = deleteOne(Branch);
