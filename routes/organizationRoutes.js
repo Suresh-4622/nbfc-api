e09812d32392c86ddeaf4dfcf20285d6e2e5f0cf
Suresh-4622 <138596848+Suresh-4622@users.noreply.github.com>
2023-10-05 14:19:52 +0530

    Update organizationRoutes.js

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -5,16 +5,14 @@ const router = Router();
 import {
   createOrganization,
   getAllOrganization,
-  getOrganization,
+  getOneOrganization,
   updateOrganization,
   deleteOrganization,
 } from "../controllers/organizationController.js";
 
 router.route("/create").post(createOrganization);
 router.route("/all/list").get(getAllOrganization);
-router
-  .route(":/id")
-  .get(getOrganization)
-  .put(updateOrganization)
-  .delete(deleteOrganization);
+router.route("/update").put(updateOrganization);
+router.route("/get/one").get(getOneOrganization);
+router.route("/delete/one").delete(deleteOrganization);
 export default router;
