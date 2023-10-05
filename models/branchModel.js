7a409532a68df13796b44527f631eaadcaf374e2
Suresh-4622 <138596848+Suresh-4622@users.noreply.github.com>
2023-10-05 15:19:07 +0530

    Update branchModel.js

--- models/branchModel.js
+++ models/branchModel.js
@@ -1,64 +1,66 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-const branchSchema = new Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    orgId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Organization",
-    },
-    branchName:{
-      type: String,
-      required: [true, "Branch Name is required"],
-    },
-    branchCode:{
-      type: String,
-      required: [true, "Branch Code is required"],
-    },
-    branchPhone:{
-        type: String,
-        required: [true, "Branch phone is required"],
-    },
-    branchEmail: {
-        type: String,
-        required: [true, "Branch email is required"],
-    },
-    address:{
-      type: String,
-      required: [true, "Branch address is required"],
-    },
-    state: {
-      type: String,
-      required: [true, "Branch state is required"],
-    },
-    city:{
-      type: String,
-      required: [true, "Branch city is required"],
-    },
-    pincode: {
-      type: Date,
-      required: [true, "Branch pincode is required"],
-    },
-    createdAt:{
-        type: Date,
-    },
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    updatedAt:{
-      type: Date,
-    },
-    updatedBy:{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
+const branchSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
-);
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+  },
+  orgId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Organization",
+  },
+  branchName: {
+    type: String,
+    required: [true, "Branch Name is required"],
+  },
+  branchCode: {
+    type: String,
+    required: [true, "Branch Code is required"],
+  },
+  branchPhone: {
+    type: String,
+    required: [true, "Branch phone is required"],
+  },
+  branchEmail: {
+    type: String,
+    required: [true, "Branch email is required"],
+  },
+  address: {
+    type: String,
+    required: [true, "Branch address is required"],
+  },
+  state: {
+    type: String,
+    required: [true, "Branch state is required"],
+  },
+  city: {
+    type: String,
+    required: [true, "Branch city is required"],
+  },
+  pincode: {
+    type: Date,
+    required: [true, "Branch pincode is required"],
+  },
+  createdAt: {
+    type: Date,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  updatedAt: {
+    type: Date,
+  },
+  updatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
 branchSchema.method("toJSON", function () {
   const { __v, _id, ...object } = this.toObject();
