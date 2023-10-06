ef6ca96fb9f81c9ebed528f99e76ea5baf82d902
Suresh-4622 <138596848+Suresh-4622@users.noreply.github.com>
2023-10-06 17:14:51 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ import User from "../models/userModel.js";
 import { TOKEN_KEY } from "../config.js";
 import moment from "moment-timezone";
 import jwt from "jsonwebtoken";
+import Client from "../models/clientModel.js";
+import Organization from "../models/orgModel.js";
+import Branch from "../models/branchModel.js";
 
 export async function userRegistration(req, res, next) {
   try {
@@ -9,7 +12,7 @@ export async function userRegistration(req, res, next) {
 
     const missingFields = [];
 
-    const requiredFields = ["userName" ,"email", "password"];
+    const requiredFields = ["userName", "email", "password"];
     for (const field of requiredFields) {
       if (!data[field]) {
         missingFields.push(field);
@@ -44,7 +47,7 @@ export async function userRegistration(req, res, next) {
 
       res.status(200).json({
         status: true,
-       message: "Register Successfully",
+        message: "Register Successfully",
       });
     } else {
       res.status(422).json({
@@ -70,7 +73,6 @@ export async function userRegistration(req, res, next) {
   }
 }
 
-
 export async function login(req, res, next) {
   try {
     const data = req.body;
@@ -95,42 +97,223 @@ export async function login(req, res, next) {
       email: data.email,
       password: data.password,
     });
-
+    const clientData = await Client.findOne({ userId: user.id });
+    const orgData = await Organization.findOne({ userId: user.id });
+    const branchData = await Branch.findOne({ userId: user.id });
     const date = Date.now();
 
     if (user) {
-      const tokenId = jwt.sign(
-        { email: data.email, password: data.password },
-        TOKEN_KEY,
-        {
-          expiresIn: "20m",
+      if (user.isFirst && !clientData) {
+        const tokenId = jwt.sign(
+          { userId: user._id, userType: user.userType },
+          TOKEN_KEY,
+          {
+            expiresIn: "20m",
+          }
+        );
+        const userData = await User.findByIdAndUpdate(
+          { _id: user._id },
+          {
+            lastLogin: date,
+            token: tokenId,
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+
+        res.status(200).json({
+          status: true,
+          message: "Login Successfully",
+          wizard: 0,
+          token: userData.token,
+        });
+      } else if (clientData && !orgData) {
+        const tokenId = jwt.sign(
+          {
+            userId: user._id,
+            clientId: clientData._id,
+            userType: user.userType,
+          },
+          TOKEN_KEY,
+          {
+            expiresIn: "20m",
+          }
+        );
+        const userData = await User.findByIdAndUpdate(
+          { _id: user._id },
+          {
+            lastLogin: date,
+            token: tokenId,
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+
+        res.status(200).json({
+          status: true,
+          message: "Login Successfully",
+          wizard: 1,
+          token: userData.token,
+        });
+      } else if (orgData && !branchData) {
+        const tokenId = jwt.sign(
+          {
+            userId: user._id,
+            clientId: clientData._id,
+            orgId: orgData._id,
+            userType: user.userType,
+          },
+          TOKEN_KEY,
+          {
+            expiresIn: "20m",
+          }
+        );
+        const userData = await User.findByIdAndUpdate(
+          { _id: user._id },
+          {
+            lastLogin: date,
+            token: tokenId,
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+
+        res.status(200).json({
+          status: true,
+          message: "Login Successfully",
+          wizard: 2,
+          token: userData.token,
+        });
+      } else if (!user.isFirst) {
+        const checkUserOrg = await Organization.find({ userId: user._id });
+        if (checkUserOrg.length === 1) {
+          const tokenId = jwt.sign(
+            {
+              userId: user._id,
+              clientId: clientData._id,
+              orgId: checkUserOrg[0]._id,
+              userType: user.userType,
+            },
+            TOKEN_KEY,
+            {
+              expiresIn: "20m",
+            }
+          );
+          const userData = await User.findByIdAndUpdate(
+            { _id: user._id },
+            {
+              lastLogin: date,
+              token: tokenId,
+            },
+            {
+              new: true,
+              runValidators: true,
+            }
+          );
+          res.status(200).json({
+            status: true,
+            message: "Logind Successfully",
+            token: userData.token,
+          });
+          //get branch details
+
+          const checkUserBranch = await Branch.find({ userId: user._id });
+          if (checkUserBranch.length === 1) {
+            const tokenId = jwt.sign(
+              {
+                userId: user._id,
+                clientId: clientData._id,
+                orgId: orgData._id,
+                branchId: checkUserBranch[0]._id,
+                userType: user.userType,
+              },
+              TOKEN_KEY,
+              {
+                expiresIn: "20m",
+              }
+            );
+            const userData = await User.findByIdAndUpdate(
+              { _id: user._id },
+              {
+                lastLogin: date,
+                token: tokenId,
+              },
+              {
+                new: true,
+                runValidators: true,
+              }
+            );
+            res.status(200).json({
+              status: true,
+              message: "Logind Successfully",
+              token: userData.token,
+            });
+          } else {
+            const tokenId = jwt.sign(
+              {
+                userId: user._id,
+                clientId: clientData._id,
+                orgId: orgData._id,
+                userType: user.userType,
+              },
+              TOKEN_KEY,
+              {
+                expiresIn: "20m",
+              }
+            );
+            const userData = await User.findByIdAndUpdate(
+              { _id: user._id },
+              {
+                lastLogin: date,
+                token: tokenId,
+              },
+              {
+                new: true,
+                runValidators: true,
+              }
+            );
+            res.status(200).json({
+              status: true,
+              message: "Logind Successfully",
+              userType: user.userType,
+            });
+          }
+        } else {
+          const tokenId = jwt.sign(
+            {
+              userId: user._id,
+              clientId: clientData._id,
+              userType: user.userType,
+            },
+            TOKEN_KEY,
+            {
+              expiresIn: "20m",
+            }
+          );
+          const userData = await User.findByIdAndUpdate(
+            { _id: user._id },
+            {
+              lastLogin: date,
+              token: tokenId,
+            },
+            {
+              new: true,
+              runValidators: true,
+            }
+          );
+          res.status(200).json({
+            status: true,
+            message: "Logind Successfully",
+            token: userData.token,
+          });
         }
-      );
-      const userData= await User.findByIdAndUpdate(
-        { _id: user._id },
-        {
-          lastLogin: date,
-          token: tokenId,
-        },
-        {
-          new: true,
-          runValidators: true,
-          select: '-password -isFirst -id',
-        }
-      );
-      const responseUserData = {
-        userName: userData.userName,
-        email: userData.email,
-        createdAt: userData.createdAt,
-        id: userData.id,
-      };
-      res.status(200).json({
-        status: true,
-        page: "wizard",
-        isFirst: user.isFirst,
-        token: userData.token,
-        userData:responseUserData,
-      });
+      }
     } else {
       res.status(422).json({
         status: false,
